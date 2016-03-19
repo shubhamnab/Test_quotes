0a0e670af5aa8068d3638a65b9d5fb5bb80e7aea
Dinesh Borana <dinesh.borana@snapdeal.com>
2016-03-19 19:51:02 +0530

    ngcopyclip board

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-var myApp=angular.module('starter.controllers', ['ngCordova'])
+var myApp=angular.module('starter.controllers', ['ngCordova','ngclipboard'])
 myApp.factory('Data',function(){
     return{
         id:''
